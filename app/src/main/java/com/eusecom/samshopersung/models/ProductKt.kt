2fb8bd7f620887bb8a48e6f4bb0137bc05d70071
eurosecom <eurosecom1@gmail.com>
2018-07-05 17:42:09 +0200

    OfferKtActivity.java
    #6
    - created OfferProductAdapter.java

--- app/src/main/java/com/eusecom/samshopersung/models/ProductKt.kt
+++ app/src/main/java/com/eusecom/samshopersung/models/ProductKt.kt
@@ -2,5 +2,5 @@ package com.eusecom.samshopersung
 
 data class ProductKt(var cis : String, var nat: String, var mer: String
                      , var cep: String, var ced: String, var dph: String, var zas: String, var cat: String
-                     , var cep2: String, var ced2: String
+                     , var cep1: String, var ced1: String
                      , var prm1: String)
