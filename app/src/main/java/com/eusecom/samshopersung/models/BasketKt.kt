1b9e318ea6c029c5d1263712c16a62723e1e1e29
eurosecom <eurosecom1@gmail.com>
2018-07-08 21:25:06 +0200

    BasketKtActivity.kt
    #8
    - created BasketKt model

---
+++ app/src/main/java/com/eusecom/samshopersung/models/BasketKt.kt
@@ -0,0 +1,6 @@
+package com.eusecom.samshopersung
+
+data class BasketKt(var xdok : String, var xice: String, var xodbm: String, var xcpl: String
+                     , var xcis: String, var xnat: String, var xdph: String, var xcep: String, var xced: String
+                     , var cmno: String, var xhdb: String, var xhdd: String, var xid: String
+                     , var pdatm: String)
